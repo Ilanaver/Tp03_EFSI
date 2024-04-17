a0b68be335a4549de5d94afc434be667f0e7e6b7
MARTIN EZEQUIEL RABER <47436029@ort>
2024-04-17 09:05:08 -0300

    empezamos nashe

---
+++ src/components/Subtitulo/Subtitulo.js
@@ -0,0 +1,13 @@
+import './Subtitulo.css';
+import React from "react"
+
+function Subtitulo() {
+    return (
+        <div>
+        <h3>CREAR MI CITA</h3>
+        <h3>ADMINISTRA TUS CITAS</h3>
+        </div>
+    )
+}
+
+export default Subtitulo;
---
+++ src/components/Titulo/Titulo.js
@@ -0,0 +1,8 @@
+import Subtitulo from '../Subtitulo/Subtitulo';
+import './Titulo.css';
+import React from "react"
+function Titulo(){
+    return <h1>ADMINISTRADOR DE PACIENTES</h1>
+}
+
+export default Titulo;
